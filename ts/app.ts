ac6b2b49ef5c4dbea7069e82d10c137d847771b8
wchang28 <wchang28@hotmail.com>
2017-07-20 07:35:53 -0400

    sgfnbhhngndgh

--- ts/app.ts
+++ ts/app.ts
@@ -1,56 +1,59 @@
-import * as mssql from "mssql";
-import * as sql from "mssql/msnodesqlv8";
+import * as sql from "mssql";
 
-let s = new Set<string>();
-s.add("wen");
-s.add("juei");
-console.log(s.has("wen"));
-
-function promiseTest() : Promise<any> {
-    return Promise.resolve<any>(null);
+function createPool(msnodesqlv8: boolean, config: sql.config) : sql.ConnectionPool {
+    if (msnodesqlv8) {
+        let nsql = require("mssql/msnodesqlv8");
+        return new nsql.ConnectionPool(config);
+    } else
+        return new sql.ConnectionPool(config);
 }
 
-/*
-let conn = new mssql.ConnectionPool({user: "wchang", password: "ts9ah7g5", server: "192.168.1.14", database: "master", connectionTimeout: 5000}, (err?: any) => {
-    if (err)
-        console.error("I am here 2. err=" + JSON.stringify(err));
-    else
-        console.log("connected :-)");
-        conn.request().query("select [time]=getdate();select [x]=1;").then((result: mssql.IResult<any>) => {
-            console.log(JSON.stringify(result.recordsets, null, 2));
-        }).catch((err: any) => {
-            console.error("I am here 3. err=" + JSON.stringify(err));
-        })
-});
-*/
+let conn = createPool(false, {server: "DESKTOP-0LO8NI0", database: "TestDB", user: "wchang", password: "ts9ah7g5"});
+//let conn = createPool(true, {server: "DESKTOP-0LO8NI0", database: "TestDB", options: {trustedConnection: true}});
 
 /*
-let conn = new sql.ConnectionPool("Driver={SQL Server Native Client 11.0};Server=192.168.1.14;Database=master;Uid=wchang;Pwd=ts9ah7g5", (err?: any) => {
-    if (err)
-        console.error("I am here 2. err=" + JSON.stringify(err));
-    else
-        console.log("connected :-)");
-        conn.request().query("select [time]=getdate();select [x]=1;").then((result: mssql.IResult<any>) => {
-            console.log(JSON.stringify(result.recordsets, null, 2));
-        }).catch((err: any) => {
-            console.error("I am here 3. err=" + JSON.stringify(err));
-        })
+conn.on("error", (err: any) => {
+    console.error("!!! connection pool error: " + JSON.stringify(err));
+}).connect()
+.then((pool: sql.ConnectionPool) => {
+    console.log("connected :-)");
+    pool.request()
+    .input('Name', "Wen Chang")
+    .input('Age', 49)
+    //.input('Male', false)
+    .output('Id', sql.VarChar(50))
+    .execute("[dbo].[stp_EchoSimpleParams]")
+    .then((result: sql.IProcedureResult<any>) => {
+        console.log(JSON.stringify(result, null, 2));
+        pool.close();
+    }).catch((err: any) => {
+        console.error("!!! query error: " + JSON.stringify(err));
+        pool.close();
+    });
+}).catch((err: any) => {
+    console.error("!!! unable to connect: " + JSON.stringify(err));
 });
 */
-
-let conn = new sql.ConnectionPool("Driver={SQL Server Native Client 11.0};Server=192.168.1.14;Database=master;Trusted_Connection=yes", (err?: any) => {
-    if (err)
-        console.error("I am here 2. err=" + JSON.stringify(err));
-    else
-        console.log("connected :-) :-)");
-        conn.request().query("select [time]=getdate();select [x]=1;").then((result: mssql.IResult<any>) => {
-            console.log(JSON.stringify(result.recordsets, null, 2));
-        }).catch((err: any) => {
-            console.error("I am here 3. err=" + JSON.stringify(err));
-        })
-});
-
 
 conn.on("error", (err: any) => {
-    console.error("I am here 1. err=" + JSON.stringify(err));
+    console.error("!!! connection pool error: " + JSON.stringify(err));
+}).connect()
+.then((pool: sql.ConnectionPool) => {
+    console.log("connected :-)");
+    let tvp = new sql.Table();
+    tvp.columns.add('Id', sql.VarChar(50));
+    tvp.rows.add("abcd");
+    tvp.rows.add("efgh");
+    pool.request()
+    .input('Ids', tvp)
+    .execute("[dbo].[stp_EchoTVP]")
+    .then((result: sql.IProcedureResult<any>) => {
+        console.log(JSON.stringify(result, null, 2));
+        pool.close();
+    }).catch((err: any) => {
+        console.error("!!! query error: " + JSON.stringify(err));
+        pool.close();
+    });
+}).catch((err: any) => {
+    console.error("!!! unable to connect: " + JSON.stringify(err));
 });
